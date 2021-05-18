4cc4285c46e1239bb94d2ea8e440fb6defea80d6
Corentin Farcy <corentinfarcy1@gmail.com>
2021-05-18 23:16:50 +0200

    Update UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
             'name' => $this->faker->firstname,
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$hb40E4RvtWhUSDr5JG6kjudqSrIrUHCjRkfzAre4rs3w.wLht4QJS', // test
             'phone' => $this->faker->phoneNumber,
             'countries_id' => $this->faker->randomDigit(),
             'created_at' => $this->faker->dateTime(),
