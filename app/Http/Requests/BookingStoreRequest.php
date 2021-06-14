b96780ca7d4e86fcd0a8464d3189d5421271c2f0
Corentin Farcy <corentinfarcy1@gmail.com>
2021-06-14 18:15:57 +0200

    Add BookingStoreRequest

---
+++ app/Http/Requests/BookingStoreRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BookingStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'started_at' => 'required',
+            'finished_at' => 'required',
+            'nb_night' => 'required',
+            'nb_adult' => 'required',
+        ];
+    }
+}
